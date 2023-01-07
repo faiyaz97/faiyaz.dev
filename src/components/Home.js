362f5acacb358c42c9b60a74f135fe2a4e258f86
faiyaz97 <faiyaz.f.uddin@gmail.com>
2023-01-07 22:52:47 +0000

    changed cat avatar and its animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
-import { useState, useEffect} from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import avatar from "../assets/image/cat-nerd.png";
+import avatar from "../assets/image/cat.png";
 import "animate.css";
 
 export const Home = () => {
@@ -14,7 +13,7 @@ export const Home = () => {
                         <h3 className="animate__animated animate__zoomInDown animate__fast">A Junior Full Stack Developer</h3>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={avatar} alt="Header Image" height= "auto" width="100%"/>
+                        <img className="animate__animated animate__fadeInRight animate__faster" src={avatar} alt="Header Image" height= "auto" width="100%"/>
                     </Col>
                 </Row>
             </Container>
