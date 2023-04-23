78099602a88aab5f18f156385c0dc17c89ea48b7
faiyaz97 <faiyaz.f.uddin@gmail.com>
2023-04-23 21:10:31 +0100

    added the new project and resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,65 @@
 import { useEffect, useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaFileAlt } from "react-icons/fa";
 import SVG from "../assets/image/FFU-02.svg";
-
+import Resume from "../assets/Resume-FFU.pdf";
 
 export const NavBar = () => {
-    const [scrolled, setScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        }
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-        window.addEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
 
-        return () => window.removeEventListener("scroll", onScroll)
-    }, [])
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-    return (
-        <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={SVG} width="30" height="30" viewBox="0 0 30 30"/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" >
-                    <span className="navbar-toggler-icon"></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    <Nav.Link href="#home" className="navbar-link">Home</Nav.Link>
-                    <Nav.Link href="#about" className="navbar-link">About</Nav.Link>
-                    <Nav.Link href="#projects" className="navbar-link">Projects</Nav.Link>
-                    <Nav.Link href="#contact" className="navbar-link">Contact</Nav.Link>
-                </Nav>
-                <span className="navbar-text">
-                    <div className="social-icon">
-                        <a href="https://linkedin.com/in/faiyaz-fu/" target='_blank'><FaLinkedin className="icons"/></a>
-                        <a href="https://github.com/faiyaz97" target='_blank'><FaGithub className="icons"/></a>
-                    </div>
-                </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={SVG} width="30" height="30" viewBox="0 0 30 30" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className="navbar-link">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#about" className="navbar-link">
+              About
+            </Nav.Link>
+            <Nav.Link href="#projects" className="navbar-link">
+              Projects
+            </Nav.Link>
+            <Nav.Link href="#contact" className="navbar-link">
+              Contact
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://linkedin.com/in/faiyaz-fu/" target="_blank">
+                <FaLinkedin className="icons" />
+              </a>
+              <a href="https://github.com/faiyaz97" target="_blank">
+                <FaGithub className="icons" />
+              </a>
+              <a href={Resume} target="_blank" rel="noopener noreferrer">
+                <FaFileAlt className="icons" />
+              </a>
+            </div>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,78 +1,127 @@
 import { Col, Container, Row } from "react-bootstrap";
-import Card from 'react-bootstrap/Card';
-import { FaGithub } from "react-icons/fa";
-import Carousel from 'react-elastic-carousel';
+import Card from "react-bootstrap/Card";
+import { FaGithub, FaYoutube, FaGlobe } from "react-icons/fa";
+import Carousel from "react-elastic-carousel";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
-
 export const Projects = () => {
+  const breakPoints = [
+    { width: 1, itemsToShow: 1, showArrows: false },
+    { width: 550, itemsToShow: 1, showArrows: false },
+    { width: 750, itemsToShow: 2, itemsToScroll: 1 },
+    { width: 1150, itemsToShow: 2, itemsToScroll: 1 },
+    { width: 1450, itemsToShow: 2, itemsToScroll: 1 },
+    { width: 1750, itemsToShow: 2, itemsToScroll: 1 },
+  ];
 
-    const breakPoints = [
-        { width: 1, itemsToShow: 1, showArrows: false  },
-        { width: 550, itemsToShow: 1, showArrows: false  },
-        { width: 750, itemsToShow: 2, itemsToScroll: 1, },
-        { width: 1150, itemsToShow: 2, itemsToScroll: 1 },
-        { width: 1450, itemsToShow: 2, itemsToScroll: 1, },
-        { width: 1750, itemsToShow: 2, itemsToScroll: 1, },
-    ]
-
-
-    return (
-        <section className="projects" id="projects">
-            <TrackVisibility once="True" partialVisibility="True">
-            {({ isVisible}) =>
-            <div className={isVisible ? "animate__animated animate__bounceInRight animate__fast" : "hide_it"}  >
+  return (
+    <section className="projects" id="projects">
+      <TrackVisibility once="True" partialVisibility="True">
+        {({ isVisible }) => (
+          <div
+            className={
+              isVisible
+                ? "animate__animated animate__bounceInRight animate__fast"
+                : "hide_it"
+            }
+          >
             <Container className="projects-container">
-                <Row>
-                    <Col>
-                        <Row className="projects-row">
-                            <h2>PROJECTS</h2>
-                        </Row>
-                        <Row  className="projects-row">
-                        <Carousel breakPoints={breakPoints}>
-                            {[
-                                {
-                                    title: 'Footy plan', 
-                                    description: 'Footy Plan is a web application to create and manage football tournaments.', 
-                                    imageUrl: require("../assets/image/footy-plan.png"),
-                                    link: 'https://github.com/faiyaz97/footy-plan'
-                                },     
-                                {
-                                    title: 'Jobd', 
-                                    description: 'An Android application where you can post and search for a job.', 
-                                    imageUrl: require("../assets/image/jobd.png"),
-                                    link: 'https://github.com/faiyaz97/Jobd'
-                                },
-                                {
-                                    title: 'Fashion MNIST AI', 
-                                    description: 'Using the Fashion MNIST dataset imported from the keras datasets and creating the best model for this type of dataset to recognize the clothing.', 
-                                    imageUrl: require("../assets/image/fashion.png"),
-                                    link: 'https://github.com/faiyaz97/fashion-MNIST-AI'
-                                },     
-                            ].map((project) => (
-                                <Col className="p-md-4">
-                                    <Card className="projects-card">
-                                        <Card.Img variant="top" src={project.imageUrl} alt='project picture' className="card-image"/>
-                                        <Card.Body>
-                                        <Card.Title>{project.title}</Card.Title>
-                                        <Card.Text>
-                                            {project.description}
-                                        </Card.Text>
-                                        <Card.Link href={project.link} title="Source code on GitHub"  target="_blank" className="card-link"><FaGithub className="project-icon"/></Card.Link>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            ))}
-                            </Carousel>
-                        </Row>
-                    </Col>
-                </Row>
+              <Row>
+                <Col>
+                  <Row className="projects-row">
+                    <h2>PROJECTS</h2>
+                  </Row>
+                  <Row className="projects-row">
+                    <Carousel breakPoints={breakPoints}>
+                      {[
+                        {
+                          title: "GlobeSnap",
+                          description:
+                            "GlobeSnap is a social network where users post pictures tagged with country locations. A world map on their profile displays visited countries in different colors.",
+                          imageUrl: require("../assets/image/globeSnap.png"),
+                          githubLink: "https://github.com/faiyaz97/GlobeSnap",
+                          youtubeLink:
+                            "https://www.youtube.com/watch?v=_duGhBK7y9k",
+                          demoLink:
+                            "https://couple-journey-frontend.onrender.com/",
+                        },
+                        {
+                          title: "Footy plan",
+                          description:
+                            "Footy Plan is a web application to create and manage football tournaments.",
+                          imageUrl: require("../assets/image/footy-plan.png"),
+                          githubLink: "https://github.com/faiyaz97/footy-plan",
+                        },
+                        {
+                          title: "Jobd",
+                          description:
+                            "An Android application where you can post and search for a job.",
+                          imageUrl: require("../assets/image/jobd.png"),
+                          githubLink: "https://github.com/faiyaz97/Jobd",
+                        },
+                        {
+                          title: "Fashion MNIST AI",
+                          description:
+                            "Using the Fashion MNIST dataset imported from the keras datasets and creating the best model for this type of dataset to recognize the clothing.",
+                          imageUrl: require("../assets/image/fashion.png"),
+                          githubLink:
+                            "https://github.com/faiyaz97/fashion-MNIST-AI",
+                        },
+                      ].map((project) => (
+                        <Col className="p-md-4">
+                          <Card className="projects-card">
+                            <Card.Img
+                              variant="top"
+                              src={project.imageUrl}
+                              alt="project picture"
+                              className="card-image"
+                            />
+                            <Card.Body>
+                              <Card.Title>{project.title}</Card.Title>
+                              <Card.Text>{project.description}</Card.Text>
+                              {project.githubLink && (
+                                <Card.Link
+                                  href={project.githubLink}
+                                  title="Source code on GitHub"
+                                  target="_blank"
+                                  className="card-link"
+                                >
+                                  <FaGithub className="project-icon" />
+                                </Card.Link>
+                              )}
+                              {project.youtubeLink && (
+                                <Card.Link
+                                  href={project.youtubeLink}
+                                  title="Youtube demo video"
+                                  target="_blank"
+                                  className="card-link"
+                                >
+                                  <FaYoutube className="project-icon" />
+                                </Card.Link>
+                              )}
+                              {project.demoLink && (
+                                <Card.Link
+                                  href={project.demoLink}
+                                  title="Demo Website"
+                                  target="_blank"
+                                  className="card-link"
+                                >
+                                  <FaGlobe className="project-icon" />
+                                </Card.Link>
+                              )}
+                            </Card.Body>
+                          </Card>
+                        </Col>
+                      ))}
+                    </Carousel>
+                  </Row>
+                </Col>
+              </Row>
             </Container>
-            </div>}
-            </TrackVisibility>
-            
-        </section>
-    )
-
-}
+          </div>
+        )}
+      </TrackVisibility>
+    </section>
+  );
+};
